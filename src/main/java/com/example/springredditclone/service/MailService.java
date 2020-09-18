d849fa659f950f90ace79d6c489dbaa29eb1f84a
ilgarbabayev <babayev.ilqar@gmail.com>
2020-09-18 19:02:12 +0300

    comments

--- src/main/java/com/example/springredditclone/service/MailService.java
+++ src/main/java/com/example/springredditclone/service/MailService.java
@@ -10,15 +10,12 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
 @Service
 @AllArgsConstructor
 @Slf4j
 class MailService {
-
     private final JavaMailSender mailSender;
     private final MailContentBuilder mailContentBuilder;
-
     @Async
     void sendMail(NotificationEmail notificationEmail) {
         MimeMessagePreparator messagePreparator = mimeMessage -> {
@@ -26,7 +23,7 @@ class MailService {
             messageHelper.setFrom("springreddit@email.com");
             messageHelper.setTo(notificationEmail.getRecipient());
             messageHelper.setSubject(notificationEmail.getSubject());
-            messageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
+            messageHelper.setText(notificationEmail.getBody());
         };
         try {
             mailSender.send(messagePreparator);
