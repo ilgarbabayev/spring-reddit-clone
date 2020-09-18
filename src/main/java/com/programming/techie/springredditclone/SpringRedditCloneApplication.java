5e504589c4ef156ad522c268dbf11c1c98004ebd
ilgarbabayev <babayev.ilqar@gmail.com>
2020-09-18 11:17:54 +0300

    add entity and repository

--- src/main/java/com/programming/techie/springredditclone/SpringRedditCloneApplication.java
+++ src/main/java/com/programming/techie/springredditclone/SpringRedditCloneApplication.java
@@ -1,4 +1,4 @@
-package com.programming.techie.springredditclone.springredditclone;
+package com.programming.techie.springredditclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
